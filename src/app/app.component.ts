1863cd6f9b9b5a02d64d16e8717b7625dee141a2
zenon2805 <portillozenon@gmail.com>
2024-01-06 12:47:37 -0600

    Se agrega barra de Navegación y botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NgbAlert, NgbToast } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlert, NgbToast, NgbCollapseModule, NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbAlert, NgbToast],
+  imports: [CommonModule, RouterOutlet, NgbAlert, NgbToast, NgbCollapseModule, NgbNavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'santiago simanca';
+  isCollapsed = false;
+  active = 1;
 }
